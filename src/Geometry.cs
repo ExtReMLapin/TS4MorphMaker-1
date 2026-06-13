7a2011f462b05e7b1f1cabce7f82c27784602240
ExtReMLapin <3909752+ExtReMLapin@users.noreply.github.com>
2026-06-13 21:59:52 +0200

    fixed Matrix4D.Inverse()

--- src/Geometry.cs
+++ src/Geometry.cs
@@ -1482,7 +1482,7 @@ namespace MorphTool
             minv[3,2] = (m[0,2]*m[1,1]*m[3,0] - m[0,1]*m[1,2]*m[3,0] - m[0,2]*m[1,0]*m[3,1] + m[0,0]*m[1,2]*m[3,1] + m[0,1]*m[1,0]*m[3,2] - m[0,0]*m[1,1]*m[3,2]) * invdet;
             minv[3,3] = (m[0,1]*m[1,2]*m[2,0] - m[0,2]*m[1,1]*m[2,0] + m[0,2]*m[1,0]*m[2,1] - m[0,0]*m[1,2]*m[2,1] - m[0,1]*m[1,0]*m[2,2] + m[0,0]*m[1,1]*m[2,2]) * invdet;
 
-            return new Matrix4D(m);
+            return new Matrix4D(minv);
         }
 
         public Matrix4D Transpose()
